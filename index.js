e9ca4cbff4d35757630ddef6b831529a83361a41
pr-0liveira <pedrorafa181818@gmail.com>
2025-07-08 11:25:50 -0300

    continuação da função de pesquisa

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const inPesquisar = document.getElementById ("inPesquisar")
-const btPesquisar = document.getElementById ("btPesquisar")
-const outResultado = document.getElementById ("outResultado")
+const inPesquisa = document.getElementById("inPesquisa")
+const btPesquisar = document.getElementById("btPesquisar")
+const outResultado = document.getElementById("outResultado")
+const sltFormadePesquisa = document.getElementById("sltFormadePesquisa")
 
 
 btPesquisar.addEventListener("click", pesquisarElementos)
 
-var vetResultado = []
-function pesquisarElementos(){
-for (let i = 0; i < vetDescricao.length; i++){
-    if(vetDescricao[i].toUpperCase().includes(inPesquisar.value.toUpperCase())){
-        vetResultado.push(vetDescricao[i])
-    }
-}
+switch ()
 
+function pesquisarElementos() {
+    var vetProdutosPesquisados = []
+    for (let i = 0; i < vetDescricao.length; i++) {
+        if (vetDescricao[i].toUpperCase().includes(inPesquisa.value.toUpperCase())) {
+            vetProdutosPesquisados.push(vetDescricao[i])
+        }
+    }
+    outResultado.innerHTML = `O produtos disponíveis são: <br> ${vetProdutosPesquisados.join("<br>")} `
 }
