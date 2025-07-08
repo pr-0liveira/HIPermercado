3c4fd1aa5321e16ca704810477427cbf3a894e1f
HenriqueHH15 <henriquehh1511@gmail.com>
2025-07-08 11:21:49 -0300

    Adicionei uma section no html e criei a listagem dos produtos pelo js

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const inPesquisar = document.getElementById ("inPesquisar")
 const btPesquisar = document.getElementById ("btPesquisar")
 const outResultado = document.getElementById ("outResultado")
 
+//gerando a lista de produtos
+var teste = document.getElementById("teste");
+for(let i = 0; i < vetDescricao.length; i++){
+    var produto = document.createElement('div');
+    var infoProduto = document.createElement('h4');
+    infoProduto.innerHTML = "Produto: " + vetDescricao[i] +
+                            "<br>Categoria: " + vetCategoria[i] +
+                            "<br>Marca: " + vetMarca[i] +
+                            "<br>Unidade de Medida: " + vetUnidMed[i] +
+                            "<br>Preço: R$" + vetPreco[i];
+    var btAddCarrinho = document.createElement('button');
+    btAddCarrinho.textContent = "Adicionar ao Carrinho";
+    produto.appendChild(infoProduto);
+    produto.appendChild(btAddCarrinho);
+    document.body.insertBefore(produto, teste);
+}
 
 btPesquisar.addEventListener("click", pesquisarElementos)
 
