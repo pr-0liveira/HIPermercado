a4c3e81c89b0121c280368426aa9f2142ac4213b
pr-0liveira <pedrorafa181818@gmail.com>
2025-07-29 10:34:35 -0300

    rápida pausa 2

--- index.js
+++ index.js
@@ -23,24 +23,23 @@ function pesquisarElementos() {
             vetCategoriaProdutosPesquisados.push(vetCategoria[i])
             vetUnidProdutosPesquisados.push(vetUnidMed[i])
             vetPreçoProdutosPesquisados.push(vetPreco[i])
-            for (let i = 0; i < vetDescricao.length; i++) {
-                var listaProdutos = document.getElementById("lista-produtos");
-                var produto = document.createElement('div');
-                produto.className = "produto";
-                var infoProduto = document.createElement('h4');
-                infoProduto.innerHTML = "Produto: " + vetDescricao[i] +
-                    "<br>Categoria: " + vetCategoria[i] +
-                    "<br>Marca: " + vetMarca[i] +
-                    "<br>Unidade de Medida: " + vetUnidMed[i] +
-                    "<br>Preço: R$" + vetPreco[i];
-                var btAddCarrinho = document.createElement('button');
-                btAddCarrinho.textContent = "Adicionar ao Carrinho";
-                btAddCarrinho.value = i;
-                produto.appendChild(infoProduto);
-                produto.appendChild(btAddCarrinho);
-                listaProdutos.appendChild(produto);
-                btAddCarrinho.addEventListener("click", adicionarNoCarrinho);
-            }
+
+            var listaProdutos = document.getElementById("lista-produtos");
+            var produto = document.createElement('div');
+            produto.className = "produto";
+            var infoProduto = document.createElement('h4');
+            infoProduto.innerHTML = "Produto: " + vetDescricao[i] +
+                "<br>Categoria: " + vetCategoria[i] +
+                "<br>Marca: " + vetMarca[i] +
+                "<br>Unidade de Medida: " + vetUnidMed[i] +
+                "<br>Preço: R$" + vetPreco[i];
+            var btAddCarrinho = document.createElement('button');
+            btAddCarrinho.textContent = "Adicionar ao Carrinho";
+            btAddCarrinho.value = i;
+            produto.appendChild(infoProduto);
+            produto.appendChild(btAddCarrinho);
+            listaProdutos.appendChild(produto);
+            btAddCarrinho.addEventListener("click", adicionarNoCarrinho);
 
             var vetCarrinho = [];
             function adicionarNoCarrinho(event) {
