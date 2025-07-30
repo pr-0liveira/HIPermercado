d3255810893cd2b0ec54dbab7ab959ac63cc560e
pr-0liveira <pedrorafa181818@gmail.com>
2025-07-30 06:55:08 -0300

    continuação do código

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
-const inPesquisa = document.getElementById("inPesquisa")
-const btPesquisar = document.getElementById("btPesquisar")
-const outResultado = document.getElementById("outResultado")
-const tbPesquisa = document.getElementById("tbPsquisa")
+const inPesquisa = document.getElementById("inPesquisa");
+const btPesquisar = document.getElementById("btPesquisar");
+const outResultado = document.getElementById("outResultado");
+const tbPesquisa = document.getElementById("tbPsquisa");
 const listaProdutos = document.getElementById("lista-produtos");
-const sltPEsquisas = document.getElementById("sltPEsquisas")
+const sltPEsquisas = document.getElementById("sltPEsquisas");
 
-btPesquisar.addEventListener("click", pesquisarElementos)
+btPesquisar.addEventListener("click", pesquisarElementos);
 
+switch (sltPEsquisas) {
+    case "produtos":
+ function pesquisarElementos() {
+            var vetProdutosPesquisados = [];
+            var vetMarcaProdutosPesquisados = [];
+            var vetCategoriaProdutosPesquisados = [];
+            var vetUnidProdutosPesquisados = [];
+            var vetPreçoProdutosPesquisados = [];
+            console.log(vetCategoriaProdutosPesquisados)
 
-function pesquisarElementos() {
-    var vetProdutosPesquisados = []
-    var vetMarcaProdutosPesquisados = []
-    var vetCategoriaProdutosPesquisados = []
-    var vetUnidProdutosPesquisados = []
-    var vetPreçoProdutosPesquisados = []
-    console.log(vetCategoriaProdutosPesquisados)
+            listaProdutos.innerHTML = "";
 
-    listaProdutos.innerHTML = "";
+            for (let i = 0; i < vetDescricao.length; i++) {
+                if (vetDescricao[i].toUpperCase().includes(inPesquisa.value.toUpperCase())) {
 
-    for (let i = 0; i < vetDescricao.length; i++) {
-        if (vetDescricao[i].toUpperCase().includes(inPesquisa.value.toUpperCase())) {
+                    vetProdutosPesquisados.push(vetDescricao[i])
+                    vetMarcaProdutosPesquisados.push(vetMarca[i])
+                    vetCategoriaProdutosPesquisados.push(vetCategoria[i])
+                    vetUnidProdutosPesquisados.push(vetUnidMed[i])
+                    vetPreçoProdutosPesquisados.push(vetPreco[i])
 
-            vetProdutosPesquisados.push(vetDescricao[i])
-            vetMarcaProdutosPesquisados.push(vetMarca[i])
-            vetCategoriaProdutosPesquisados.push(vetCategoria[i])
-            vetUnidProdutosPesquisados.push(vetUnidMed[i])
-            vetPreçoProdutosPesquisados.push(vetPreco[i])
-            
-            var produto = document.createElement('div');
-            produto.className = "produto";
-            var infoProduto = document.createElement('h4');
-            infoProduto.innerHTML = "Produto: " + vetDescricao[i] +
-                "<br>Categoria: " + vetCategoria[i] +
-                "<br>Marca: " + vetMarca[i] +
-                "<br>Unidade de Medida: " + vetUnidMed[i] +
-                "<br>Preço: R$" + vetPreco[i];
-            var btAddCarrinho = document.createElement('button');
-            btAddCarrinho.textContent = "Adicionar ao Carrinho";
-            btAddCarrinho.value = i;
-            produto.appendChild(infoProduto);
-            produto.appendChild(btAddCarrinho);
-            listaProdutos.appendChild(produto);
-            btAddCarrinho.addEventListener("click", adicionarNoCarrinho);
+                    var produto = document.createElement('div');
+                    produto.className = "produto";
+                    var infoProduto = document.createElement('h4');
+                    infoProduto.innerHTML = "Produto: " + vetDescricao[i] +
+                        "<br>Categoria: " + vetCategoria[i] +
+                        "<br>Marca: " + vetMarca[i] +
+                        "<br>Unidade de Medida: " + vetUnidMed[i] +
+                        "<br>Preço: R$" + vetPreco[i];
+                    var btAddCarrinho = document.createElement('button');
+                    btAddCarrinho.textContent = "Adicionar ao Carrinho";
+                    btAddCarrinho.value = i;
+                    produto.appendChild(infoProduto);
+                    produto.appendChild(btAddCarrinho);
+                    listaProdutos.appendChild(produto);
+                    btAddCarrinho.addEventListener("click", adicionarNoCarrinho);
 
-            var vetCarrinho = [];
-            function adicionarNoCarrinho(event) {
-                vetCarrinho.push(Number(event.target.value));
-                console.log(vetCarrinho);
+                    var vetCarrinho = [];
+                    function adicionarNoCarrinho(event) {
+                        vetCarrinho.push(Number(event.target.value));
+                        console.log(vetCarrinho);
+                    }
+
+                    break;
+                }
             }
 
-
         }
-    }
 
 }
+
+
+
 
